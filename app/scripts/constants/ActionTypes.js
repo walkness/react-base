d86eaf6fe03d5efed154a06343ef260726d0bb13
Walker Angell <w@lker.co>
2017-01-05 18:34:21 -0500

    update packages and lint changes

--- app/scripts/constants/ActionTypes.js
+++ app/scripts/constants/ActionTypes.js
@@ -6,6 +6,6 @@ export const FAILURE = 'FAILURE';
 
 function createRequestTypes(base) {
   const res = {};
-  [REQUEST, SUCCESS, FAILURE].forEach(type => { res[type] = `${base}_${type}`; });
+  [REQUEST, SUCCESS, FAILURE].forEach((type) => { res[type] = `${base}_${type}`; });
   return res;
 }
